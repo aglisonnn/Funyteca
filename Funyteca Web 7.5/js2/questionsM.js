644bb2e436dfc06ca049f1315b23baac54331a95
Lowfrei <112555795+Lowfrei@users.noreply.github.com>
2022-11-30 13:15:24 -0400

    questões de quizz

---
+++ Funyteca Web 7.5/js2/questionsM.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Se Maria gastou em compras 1/3 de 1/4 de R$ 300, quanto sobrou desse total?",
+    answer: "R$ 25,00",
+    options: [
+      "R$ 30,00",
+      "R$ 26,00",
+      "R$ 25,00",
+      "R$ 24,00"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Se optarmos comprar 1/2 dúzia de ovos, sendo que uma dúzia custa R$ 3,40, quanto pagaremos?",
+    answer: "R$ 1,70",
+    options: [
+      "R$ 1,90",
+      "R$ 1,70",
+      "R$ 1,50",
+      "R$ 1,40"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Qual a fração cujo denominador é igual ao dobro de uma dúzia, e o numerador é igual a meia dúzia?",
+    answer: "6/24",
+    options: [
+      "6/12",
+      "12/6",
+      "6/24",
+      "24/6"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Numa escola há 360 alunos. Calcule 5/6 desses alunos. A resposta correta é:",
+    answer: "300",
+    options: [
+      "100",
+      "150",
+      "250",
+      "300"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Um grupo de 20 turistas é formado por 9 chilenos, 5 americanos, e os demais são brasileiros. Qual a fração que do grupo representado pelos brasileiros?",
+    answer: "6/20",
+    options: [
+      "14/20",
+      "9/20",
+      "6/20",
+      "5/20"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
